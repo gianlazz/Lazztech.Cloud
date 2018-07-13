272e0152ac1b944f9f184c02e1d8fcc0460a7ba3
Gian Lazzarini <gianlazzarini@gmail.com>
2018-07-12 22:26:15 -0700

    Testing image drawing library

--- Lazztech.ObsidianPresense.Vision.Microservice.Cli/Program.cs
+++ Lazztech.ObsidianPresense.Vision.Microservice.Cli/Program.cs
@@ -4,6 +4,11 @@ using System.IO;
 using System.Linq;
 using Lazztech.ObsidianPresense.Vision.Microservice.GoogleCloudVision;
 using Newtonsoft.Json;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Processing.Filters;
+using SixLabors.ImageSharp.Processing.Transforms;
 
 namespace Lazztech.ObsidianPresense.Vision.Microservice.Cli
 {
@@ -29,8 +34,19 @@ namespace Lazztech.ObsidianPresense.Vision.Microservice.Cli
             var snapshot = gcv.Process(base64);
 
             var json = JsonConvert.SerializeObject(snapshot, Formatting.Indented);
-
             Console.WriteLine(json);
+
+            Console.WriteLine("Drawing bounding boxes.");
+
+            // Image.Load(string path) is a shortcut for our default type. Other pixel formats use Image.Load<TPixel>(string path))
+            using (Image<Rgba32> image = Image.Load(inputPath))
+            {
+                image.Mutate(x => x
+                     .Resize(image.Width / 2, image.Height / 2)
+                     .Grayscale());
+                image.Save(inputPath.TrimEnd(".jpg".ToCharArray()) + "_bar.jpg"); // automatic encoder selected based on extension.
+            }
+
             Console.ReadKey();
         }
     }
