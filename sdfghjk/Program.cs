3df2cca5dd3b2355f43e3fbc474c313017d41e90
Gian Lazzarini <gianlazzarini@gmail.com>
2018-07-14 21:02:10 -0700

    vision cli docker file run lines for python version

---
+++ sdfghjk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sdfghjk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
