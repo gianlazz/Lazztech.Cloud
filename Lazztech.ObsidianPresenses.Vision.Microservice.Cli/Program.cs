dc81e4f0b6f079ff0cceee034f17891dfb61c867
Gian Lazzarini <gianlazzarini@gmail.com>
2018-08-01 14:30:44 -0700

    solution builds again

--- Lazztech.ObsidianPresenses.Vision.Microservice.Cli/Program.cs
+++ Lazztech.ObsidianPresenses.Vision.Microservice.Cli/Program.cs
@@ -26,7 +26,7 @@ namespace Lazztech.ObsidianPresenses.Vision.Microservice.Cli
                 return;
             }
 
-            var facialRecognition = new FacialRecognitionManager();
+            var facialRecognition = new FacialRecognitionManager(new FaceRecognitionProcess());
             facialRecognition.Process();
 
             foreach (var snapshot in facialRecognition.Results)
