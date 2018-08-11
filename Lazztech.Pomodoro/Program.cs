378c70294a97644d684df8ef2f7a5ebc031bdd4e
Gian Lazzarini <gianlazzarini@gmail.com>
2018-08-11 13:40:31 -0700

    Configured launch and build preLaunchTask for solution/pomodoro

--- Lazztech.Pomodoro/Program.cs
+++ Lazztech.Pomodoro/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Lazztech.Pomodoro
 {
@@ -6,7 +7,25 @@ namespace Lazztech.Pomodoro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args[0].ToLower() == "start")
+            {}
+
+            if (args[0].ToLower() == "stop")
+            {}
+
+            if (args[0].ToLower() == "pause")
+            {}
+
+            if (args[0] == "-h")
+            {}
+
+            //Show status with brief intro and description
+            Console.WriteLine("Lazztech.Pomodoro");
+            System.Console.WriteLine("The arg -h opens help.");
+            System.Console.Write("What would you like to do: ");
+            var result = Console.Read();
+
+            System.Console.WriteLine(result);
         }
     }
 }
