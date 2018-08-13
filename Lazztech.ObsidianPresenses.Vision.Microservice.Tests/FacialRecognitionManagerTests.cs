3009ff707de37f1bd5a749930d16f5fbae08c564
Gian Lazzarini <gianlazzarini@gmail.com>
2018-08-12 20:18:36 -0700

    Caused test cases to all fail. Should probably break out the test data.

--- Lazztech.ObsidianPresenses.Vision.Microservice.Tests/FacialRecognitionManagerTests.cs
+++ Lazztech.ObsidianPresenses.Vision.Microservice.Tests/FacialRecognitionManagerTests.cs
@@ -32,18 +32,14 @@ namespace Lazztech.ObsidianPresenses.Vision.Microservice.Tests
 /face/unknown/images.jpeg,Gian Lazzarini
 /face/unknown/2892.png,Scott Hanselman
 /face/unknown/Chad Peterson.jpg,unknown_person
-/face/unknown/unnamed.jpg,Gian Lazzarini
-/face/unknown/harry-meghan-15.jpg,Meghan Markle
-/face/unknown/harry-meghan-15.jpg,Prince Harry";
+/face/unknown/unnamed.jpg,Gian Lazzarini";
 
         public static string face_detectionLinesTestData = @"/face/unknown/0.jpeg,29,133,101,61
 /face/unknown/webcam.jpg,185,400,400,185
 /face/unknown/images.jpeg,54,181,158,77
 /face/unknown/2892.png,136,394,394,136
 /face/unknown/Chad Peterson.jpg,113,328,328,113
-/face/unknown/unnamed.jpg,156,610,527,238
-/face/unknown/harry-meghan-15.jpg,294,792,443,642
-/face/unknown/harry-meghan-15.jpg,154,652,333,473";
+/face/unknown/unnamed.jpg,156,610,527,238";
         #endregion
 
         #region test cases
