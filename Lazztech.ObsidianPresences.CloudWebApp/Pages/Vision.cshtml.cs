3b8793ca809bf7180635e9a525f5253d435a42e6
Gian Lazzarini <gianlazzarini@gmail.com>
2018-09-03 11:36:35 -0700

    Replaced snapshot json stuff in Vision page with CallSnapsEndpoint()

--- Lazztech.ObsidianPresences.CloudWebApp/Pages/Vision.cshtml.cs
+++ Lazztech.ObsidianPresences.CloudWebApp/Pages/Vision.cshtml.cs
@@ -16,57 +16,20 @@ namespace Lazztech.ObsidianPresences.CloudWebApp.Pages
     {
         //public List<Snapshot> Snapshots => new List<Snapshot>();
         public List<Snapshot> Snapshots { get; set; }
-        // public List<Snapshot> Snaps { get; set; }
 
         public void OnGet()
         {
             Snapshots = new List<Snapshot>();
 
-            var resultsDir = @"/face/results/";
-            var dirExists = Directory.Exists(resultsDir);
-
-            var faceFolders = Directory.GetDirectories("/face/");
-            var knownImageDirs = Directory.GetFiles("/face/known/");
-            var unknownImageDirs = Directory.GetFiles("/face/unknown");
-
-            var jsonDirs = Directory.GetFiles(resultsDir).Where(x => x.EndsWith(".json"));
-            foreach (var jsonDir in jsonDirs)
-            {
-                var json = System.IO.File.ReadAllText(jsonDir);
-                var snapshotObject = JsonConvert.DeserializeObject(json);
-                var snapshot = JsonConvert.DeserializeObject<Snapshot>(json);
-                var imageFound = System.IO.File.Exists(snapshot.ImageDir);
-                var imageBytes = System.IO.File.ReadAllBytes(snapshot.ImageDir);
-                var imageBase64 = Convert.ToBase64String(imageBytes);
-                var imageExtension = snapshot.ImageDir.Split('.').Last();
-                snapshot.ImageDir = $"data:image/{imageExtension};base64, {imageBase64}";
-                Snapshots.Add(snapshot);
-            }
-        }
-
-        public void HitRestPoint()
-        {
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri("http://localhost:9000/");
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-                // New code:
-                HttpResponseMessage response = await client.GetAsync("api/products/1");
-                if (response.IsSuccessStatusCode)
-                {
-                    Product product = await response.Content.ReadAsAsync<Product>();
-                    Console.WriteLine("{0}\t${1}\t{2}", product.Name, product.Price, product.Category);
-                }
-            }
+            var snaps = CallSnapsEndpoint();
+            Snapshots = snaps.Result;
         }
 
         //Hosted web API REST Service base url  
         string Baseurl = "http://192.168.95.1:5555/";
-        public async Task<ActionResult> Index()
+        private async Task<List<Snapshot>> CallSnapsEndpoint()
         {
-            List<Employee> EmpInfo = new List<Employee>();
+            List<Snapshot> snaps = new List<Snapshot>();
 
             using (var client = new HttpClient())
             {
@@ -87,13 +50,58 @@ namespace Lazztech.ObsidianPresences.CloudWebApp.Pages
                     var EmpResponse = Res.Content.ReadAsStringAsync().Result;
 
                     //Deserializing the response recieved from web api and storing into the Employee list  
-                    EmpInfo = JsonConvert.DeserializeObject<List<Employee>>(EmpResponse);
+                    snaps = JsonConvert.DeserializeObject<List<Snapshot>>(EmpResponse);
 
                 }
                 //returning the employee list to view  
-                return View(EmpInfo);
+                return snaps;
             }
         }
+
+        //public void OnGet()
+        //{
+        //    Snapshots = new List<Snapshot>();
+
+        //    var resultsDir = @"/face/results/";
+        //    var dirExists = Directory.Exists(resultsDir);
+
+        //    var faceFolders = Directory.GetDirectories("/face/");
+        //    var knownImageDirs = Directory.GetFiles("/face/known/");
+        //    var unknownImageDirs = Directory.GetFiles("/face/unknown");
+
+        //    var jsonDirs = Directory.GetFiles(resultsDir).Where(x => x.EndsWith(".json"));
+        //    foreach (var jsonDir in jsonDirs)
+        //    {
+        //        var json = System.IO.File.ReadAllText(jsonDir);
+        //        var snapshotObject = JsonConvert.DeserializeObject(json);
+        //        var snapshot = JsonConvert.DeserializeObject<Snapshot>(json);
+        //        var imageFound = System.IO.File.Exists(snapshot.ImageDir);
+        //        var imageBytes = System.IO.File.ReadAllBytes(snapshot.ImageDir);
+        //        var imageBase64 = Convert.ToBase64String(imageBytes);
+        //        var imageExtension = snapshot.ImageDir.Split('.').Last();
+        //        snapshot.ImageDir = $"data:image/{imageExtension};base64, {imageBase64}";
+        //        Snapshots.Add(snapshot);
+        //    }
+        //}
+
+        //public void HitRestPoint()
+        //{
+        //    using (var client = new HttpClient())
+        //    {
+        //        client.BaseAddress = new Uri("http://localhost:9000/");
+        //        client.DefaultRequestHeaders.Accept.Clear();
+        //        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        //        // New code:
+        //        HttpResponseMessage response = await client.GetAsync("api/products/1");
+        //        if (response.IsSuccessStatusCode)
+        //        {
+        //            Product product = await response.Content.ReadAsAsync<Product>();
+        //            Console.WriteLine("{0}\t${1}\t{2}", product.Name, product.Price, product.Category);
+        //        }
+        //    }
+        //}
+
     }
 }
-}
+
