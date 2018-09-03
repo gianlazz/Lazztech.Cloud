176c23adbaf30499779125efec6e40854c41ab44
Gian Lazzarini <gianlazzarini@gmail.com>
2018-09-03 11:26:00 -0700

    Begining to implement rest api from Vision.cshtml.cs

--- Lazztech.ObsidianPresences.CloudWebApp/Pages/Vision.cshtml.cs
+++ Lazztech.ObsidianPresences.CloudWebApp/Pages/Vision.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Lazztech.ObsidianPresences.Vision.Microservice.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -41,5 +43,57 @@ namespace Lazztech.ObsidianPresences.CloudWebApp.Pages
                 Snapshots.Add(snapshot);
             }
         }
+
+        public void HitRestPoint()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:9000/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                // New code:
+                HttpResponseMessage response = await client.GetAsync("api/products/1");
+                if (response.IsSuccessStatusCode)
+                {
+                    Product product = await response.Content.ReadAsAsync<Product>();
+                    Console.WriteLine("{0}\t${1}\t{2}", product.Name, product.Price, product.Category);
+                }
+            }
+        }
+
+        //Hosted web API REST Service base url  
+        string Baseurl = "http://192.168.95.1:5555/";
+        public async Task<ActionResult> Index()
+        {
+            List<Employee> EmpInfo = new List<Employee>();
+
+            using (var client = new HttpClient())
+            {
+                //Passing service base url  
+                client.BaseAddress = new Uri(Baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format  
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+                HttpResponseMessage Res = await client.GetAsync("api/Employee/GetAllEmployees");
+
+                //Checking the response is successful or not which is sent using HttpClient  
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api   
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the Employee list  
+                    EmpInfo = JsonConvert.DeserializeObject<List<Employee>>(EmpResponse);
+
+                }
+                //returning the employee list to view  
+                return View(EmpInfo);
+            }
+        }
     }
+}
 }
