eabcf351831bef281a9078b2850f05fbc2148dbd
Gian Lazzarini <gianlazzarini@gmail.com>
2018-09-03 17:11:21 -0700

    docker-compose depends_on: field

--- Lazztech.ObsidianPresences.CloudWebApp/Pages/Vision.cshtml.cs
+++ Lazztech.ObsidianPresences.CloudWebApp/Pages/Vision.cshtml.cs
@@ -29,7 +29,7 @@ namespace Lazztech.ObsidianPresences.CloudWebApp.Pages
         //string Baseurl = "http://localhost:8080/";
         //string Baseurl = "http://localhost:50199/";
         //string Baseurl = "http://lazztechobsidianpresensevisionmicroservicewebapi:50199/";
-        string Baseurl = "http://lazztech.ObsidianPresences.vision.microservice.webapi:50199/";
+        string Baseurl = "http://lazztech.ObsidianPresences.vision.microservice.webapi:8080/";
         //string Baseurl = "http://172.20.0.3:5000/";
         //string Baseurl = "http://dockercompose18306792969269339587_lazztech.ObsidianPresences.vision.microservice.webapi_1:8080";
         private async Task<List<Snapshot>> CallSnapsEndpoint()
