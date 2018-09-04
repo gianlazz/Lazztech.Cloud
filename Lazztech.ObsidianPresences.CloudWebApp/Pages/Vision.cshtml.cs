838331ee9758c3ada5593d6c6523fe577360cfd0
Gian Lazzarini <gianlazzarini@gmail.com>
2018-09-03 18:06:06 -0700

    Weird, now it's not working again.

--- Lazztech.ObsidianPresences.CloudWebApp/Pages/Vision.cshtml.cs
+++ Lazztech.ObsidianPresences.CloudWebApp/Pages/Vision.cshtml.cs
@@ -32,7 +32,8 @@ namespace Lazztech.ObsidianPresences.CloudWebApp.Pages
         //string Baseurl = "http://lazztech.ObsidianPresences.vision.microservice.webapi:8080/";
         //string Baseurl = "http://172.20.0.3:5000/";
         //string Baseurl = "http://dockercompose18306792969269339587_lazztech.ObsidianPresences.vision.microservice.webapi_1:8080/";
-        string Baseurl = "http://c29edb6f84c8/";
+        //string Baseurl = "http://c29edb6f84c8/";
+        string Baseurl = "http://lazztech.ObsidianPresences.vision.microservice.webapi/";
         private async Task<List<Snapshot>> CallSnapsEndpoint()
         {
             List<Snapshot> snaps = new List<Snapshot>();
