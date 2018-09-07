20b5226df175b56a05fc8048fc02fe09c54eddc9
Gian Lazzarini <gianlazzarini@gmail.com>
2018-09-07 14:00:36 -0700

    Filling out the Known page with Known snapshot results.

--- Lazztech.ObsidianPresences.CloudWebApp/Pages/Vision/Index.cshtml.cs
+++ Lazztech.ObsidianPresences.CloudWebApp/Pages/Vision/Index.cshtml.cs
@@ -53,7 +53,7 @@ namespace Lazztech.ObsidianPresences.CloudWebApp.Pages.Vision
                     snaps = JsonConvert.DeserializeObject<List<Snapshot>>(EmpResponse);
                 }
                 //returning the employee list to view
-                return snaps;
+                return snaps.Where(x => x.ImageDir.Contains("/unknown/")).ToList();
             }
         }
     }
