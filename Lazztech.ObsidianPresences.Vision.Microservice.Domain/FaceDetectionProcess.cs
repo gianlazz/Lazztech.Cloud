df1b56b0200effcd02987f7f093663df6b78a6e8
Gian Lazzarini <gianlazzarini@gmail.com>
2018-09-07 22:55:20 -0700

    Implemented nonblocking event handeling for stdout & stderr streams.

--- Lazztech.ObsidianPresences.Vision.Microservice.Domain/FaceDetectionProcess.cs
+++ Lazztech.ObsidianPresences.Vision.Microservice.Domain/FaceDetectionProcess.cs
@@ -6,27 +6,44 @@ namespace Lazztech.ObsidianPresences.Vision.Microservice.Domain
 {
     public class FaceDetectionProcess : Iface_detection
     {
+        public Process process { get; private set; }
+        public List<string> result { get; private set; }
+
         public List<string> FaceDetection()
         {
-            var result = new List<string>();
+            result = new List<string>();
 
             var procInfo = new ProcessStartInfo($"face_detection")
             {
                 RedirectStandardOutput = true,
                 Arguments = $"{FacialRecognitionManager.unknownPath}"
             };
-            var proc = new Process { StartInfo = procInfo };
-
-            proc.Start();
-            while (proc.StandardOutput.EndOfStream == false)
-            {
-                var line = proc.StandardOutput.ReadLine();
-                if (string.IsNullOrEmpty(line) == false)
-                    result.Add(line);
-                Console.WriteLine(line);
-            }
+            process = new Process { StartInfo = procInfo };
+            process.OutputDataReceived += new DataReceivedEventHandler(OnProcOutputDataRecieved);
+            process.ErrorDataReceived += new DataReceivedEventHandler(OnProcErrorDataRecieved);
+            process.Start();
+            //while (process.StandardOutput.EndOfStream == false)
+            //{
+            //    var line = process.StandardOutput.ReadLine();
+            //    if (string.IsNullOrEmpty(line) == false)
+            //        result.Add(line);
+            //    Console.WriteLine(line);
+            //}
 
             return result;
+        }
+
+        private void OnProcOutputDataRecieved(object sender, DataReceivedEventArgs e)
+        {
+            var line = e.Data;
+            if (string.IsNullOrEmpty(line) == false)
+                result.Add(line);
+            Console.WriteLine(line);
+        }
+
+        private void OnProcErrorDataRecieved(object sender, DataReceivedEventArgs e)
+        {
+            throw new Exception($"{e.Data}");
         }
     }
 }
--- Lazztech.ObsidianPresences.Vision.Microservice.Domain/FaceRecognitionProcess.cs
+++ Lazztech.ObsidianPresences.Vision.Microservice.Domain/FaceRecognitionProcess.cs
@@ -6,29 +6,47 @@ namespace Lazztech.ObsidianPresences.Vision.Microservice.Domain
 {
     public class FaceRecognitionProcess : Iface_recognition
     {
+        public Process process { get; private set; }
+        public List<string> results { get; private set; }
+
         public List<string> FaceRecognition()
         {
-            var results = new List<string>();
+            results = new List<string>();
 
-            var x = Environment.ProcessorCount;
+            //var x = Environment.ProcessorCount;
 
             var procInfo = new ProcessStartInfo($"face_recognition")
             {
                 RedirectStandardOutput = true,
                 Arguments = $"{FacialRecognitionManager.knownPath} {FacialRecognitionManager.unknownPath}"
             };
-            var proc = new Process { StartInfo = procInfo };
+            process = new Process { StartInfo = procInfo };
+            process.OutputDataReceived += new DataReceivedEventHandler(OnProcOutputDataRecieved);
+            process.ErrorDataReceived += new DataReceivedEventHandler(OnProcErrorDataRecieved);
 
-            proc.Start();
-            while (proc.StandardOutput.EndOfStream == false)
-            {
-                var line = proc.StandardOutput.ReadLine();
-                if (string.IsNullOrEmpty(line) == false)
-                    results.Add(line);
-                Console.WriteLine(line);
-            }
+            process.Start();
+            //while (process.StandardOutput.EndOfStream == false)
+            //{
+            //    var line = process.StandardOutput.ReadLine();
+            //    if (string.IsNullOrEmpty(line) == false)
+            //        results.Add(line);
+            //    Console.WriteLine(line);
+            //}
 
             return results;
+        }
+
+        private void OnProcOutputDataRecieved(object sender, DataReceivedEventArgs e)
+        {
+            var line = e.Data;
+            if (string.IsNullOrEmpty(line) == false)
+                results.Add(line);
+            Console.WriteLine(line);
+        }
+
+        private void OnProcErrorDataRecieved(object sender, DataReceivedEventArgs e)
+        {
+            throw new Exception(e.Data);
         }
     }
 }
