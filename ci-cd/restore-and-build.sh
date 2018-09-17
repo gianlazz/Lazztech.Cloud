3ec2689662155723ad84c6092cd001351744cfc7
Gian Lazzarini <gianlazzarini@gmail.com>
2018-09-17 00:05:12 -0700

    Fixing build shell script single responsability.

--- ci-cd/restore-and-build.sh
+++ ci-cd/restore-and-build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-echo "Removing CLI breaking .dcproj from solution."
-dotnet sln ../Lazztech.ObsidianPresences.sln remove ../docker-compose.dcproj
+#echo "Removing CLI breaking .dcproj from solution."
+#dotnet sln ../Lazztech.ObsidianPresences.sln remove ../docker-compose.dcproj
 echo "Running dotnet restore."
 dotnet restore ../Lazztech.ObsidianPresences.sln
 echo "Running dotnet build."
