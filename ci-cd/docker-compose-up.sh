c5ea5b0387dc395baf8e936b153e78aca3043de7
Gian Lazzarini <gianlazzarini@gmail.com>
2018-09-18 19:17:35 -0700

    fixing docker-compose-up.sh

--- ci-cd/docker-compose-up.sh
+++ ci-cd/docker-compose-up.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-                                                                                                                                        'echo "Running unit tests."
+
 docker-compose up
