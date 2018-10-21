9be72834dd3560002419fbdbc1dbe77bc4eb905c
Gian Lazzarini <gianlazzarini@gmail.com>
2018-10-21 12:38:01 -0700

    Added upload page to ClientFacade vision section

---
+++ Lazztech.ObsidianPresences.ClientFacade/Pages/Vision/Upload.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lazztech.ObsidianPresences.Vision.Microservice.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lazztech.ObsidianPresences.ClientFacade.Pages.Vision
+{
+    public class UploadModel : PageModel
+    {
+        public Person Person { get; set; }
+        public string Base64Photo { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
