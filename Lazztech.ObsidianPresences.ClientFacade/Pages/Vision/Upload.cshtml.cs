eb787ccfab7c9be68d49059f316d9f581dd79de6
Gian Lazzarini <gianlazzarini@gmail.com>
2018-10-22 19:34:56 -0700

    Posting uploaded image to vison service's AddNewPerson webapi controller

--- Lazztech.ObsidianPresences.ClientFacade/Pages/Vision/Upload.cshtml.cs
+++ Lazztech.ObsidianPresences.ClientFacade/Pages/Vision/Upload.cshtml.cs
@@ -4,6 +4,8 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Lazztech.ObsidianPresences.Vision.Microservice.Domain.Models;
 using Microsoft.AspNetCore.Http;
@@ -14,9 +16,11 @@ namespace Lazztech.ObsidianPresences.ClientFacade.Pages.Vision
 {
     public class UploadModel : PageModel
     {
-        public Person Person { get; set; }
-        public Snapshot Snapshot { get; set; }
+        public string Name { get; set; }
         public IFormFile Photo { get; set; }
+        public string ImageBase64 { get; private set; }
+
+        //public bool ConnectedToServices = false;
 
         public void OnGet()
         {
@@ -25,41 +29,61 @@ namespace Lazztech.ObsidianPresences.ClientFacade.Pages.Vision
 
         public async Task<IActionResult> OnPost()
         {
-            Snapshot = new Snapshot();
-            //Customer = new Customer();
-
             if (!ModelState.IsValid)
             {
                 return Page();
             }
 
-            //Customer.PhotePath = Customer.Phote.FileName;
             await UploadPhoto();
-            //_db.Customers.Add(Customer);
-            //await _db.SaveChangesAsync();
-
             //Message = "New customer created successfully!";
 
-            return RedirectToPage("./Index");
+            return RedirectToPage("./Known");
         }
 
         private async Task UploadPhoto()
         {
-            //var uploadsDirectoryPath = Path.Combine(_environment.WebRootPath, "Uploads");
-            //var uploadsDirectoryPath = Path.Combine(_environment.WebRootPath, "Uploads");
-
-            //var uploadedfilePath = Path.Combine(uploadsDirectoryPath, Customer.Phote.FileName);
-
-            //using (var fileStream = new FileStream(uploadedfilePath, FileMode.Create))
-            //{
-            //    await Customer.Phote.CopyToAsync(fileStream);
-            //}
-
             using (var ms = new MemoryStream())
             {
                 await Photo.CopyToAsync(ms);
                 var imageBytes = ms.ToArray();
-                var imageBase64 = Convert.ToBase64String(imageBytes);
+                ImageBase64 = Convert.ToBase64String(imageBytes);
+            }
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    //Passing service base url
+                    client.BaseAddress = new Uri(StaticStrings.VisionWebapiService);
+
+                    client.DefaultRequestHeaders.Clear();
+                    //Define request data format
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    //Sending request to find web api REST service resource GetAllEmployees using HttpClient
+                    var content = new FormUrlEncodedContent(new[]
+                    {
+                        new KeyValuePair<string, string>("base64Image", ImageBase64),
+                        new KeyValuePair<string, string>("name", Name)
+                    });
+                    var Res = await client.PostAsync("api/AddNewPerson", content);
+
+                    //Checking the response is successful or not which is sent using HttpClient
+                    if (Res.IsSuccessStatusCode)
+                    {
+                        //Storing the response details recieved from web api
+                        var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+
+                        //Deserializing the response recieved from web api and storing into the Employee list
+                    }
+
+                    //ConnectedToServices = true;
+                }
+            }
+            catch (Exception)
+            {
+
+                //throw;
             }
 
             //HERE I WOULD BASE64 ENCODE THE FILESTREAM AND POST IT TO THE VISION SERVICE'S NEW ADDNEWPERSON API CONTROLLER
