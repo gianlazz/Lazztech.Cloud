b792960d0e603c7682a5904aff854c5ad2571ca4
Gian Lazzarini <gianlazzarini@gmail.com>
2018-11-02 20:03:19 -0700

    Stubbed Snapshot page

---
+++ Lazztech.ObsidianPresences.ClientFacade/Pages/Vision/Snapshot.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lazztech.ObsidianPresences.Vision.Microservice.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lazztech.ObsidianPresences.ClientFacade.Pages.Vision
+{
+    public class SnapshotModel : PageModel
+    {
+        public Snapshot Snapshot { get; set; }
+
+        public void OnGet(string id)
+        {
+
+        }
+    }
+}
