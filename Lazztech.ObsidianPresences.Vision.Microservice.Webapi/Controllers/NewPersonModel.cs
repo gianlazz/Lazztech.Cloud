4dd166789b1181258a501beba8449ccd3e196576
Gian Lazzarini <gianlazzarini@gmail.com>
2018-11-16 20:12:34 -0800

    Deleted the vision microservice webapi project from the solution

--- Lazztech.ObsidianPresences.Vision.Microservice.Webapi/Controllers/NewPersonModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Lazztech.ObsidianPresences.Vision.Microservice.Webapi.Controllers
-{
-    public class NewPersonModel
-    {
-        public string Name { get; set; }
-        public string Base64Image { get; set; }
-    }
-}
