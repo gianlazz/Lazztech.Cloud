339a403b9c6a5cda79b26428356a5510f15afa49
Gian Lazzarini <gianlazzarini@gmail.com>
2018-11-16 18:47:08 -0800

    Switched temporarily back to the default Program.cs code

--- Lazztech.ObsidianPresences.ClientFacade/Program.cs
+++ Lazztech.ObsidianPresences.ClientFacade/Program.cs
@@ -13,57 +13,58 @@ using Microsoft.Extensions.Logging;
 
 namespace Lazztech.ObsidianPresences.ClientFacade
 {
-    //public class Program
-    //{
-    //    public static void Main(string[] args)
-    //    {
-    //        CreateWebHostBuilder(args).Build().Run();
-    //    }
-
-    //    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-    //        WebHost.CreateDefaultBuilder(args)
-    //            .UseStartup<Startup>();
-    //}
-
     public class Program
     {
-        public static int Main(string[] args)
+        public static void Main(string[] args)
         {
-            try
-            {
-                var host = BuildWebHost(args);
-                using (var scope = host.Services.CreateScope())
-                {
-                    var services = scope.ServiceProvider;
-                    try
-                    {
-                        InitializeDatabase(services);
-                    }
-                    catch (Exception ex)
-                    {
-                        // something bad happened
-                    }
-                }
-                host.Run();
-                return 0;
-            }
-            catch (Exception ex)
-            {
-                return 1;
-            }
+            CreateWebHostBuilder(args).Build().Run();
         }
-        public static IWebHost BuildWebHost(string[] args) =>
-        WebHost.CreateDefaultBuilder(args)
-        .UseStartup<Startup>()
-        .UseUrls("http://*:5000")
-        .Build();
-        private static void InitializeDatabase(IServiceProvider services)
-        {
-            using (var serviceScope = services.GetService<IServiceScopeFactory>().CreateScope())
-            {
-                var context = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-                context.Database.Migrate();
-            }
-        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
     }
+
+    //public class Program
+    //{
+    //    public static int Main(string[] args)
+    //    {
+    //        try
+    //        {
+    //            var host = BuildWebHost(args);
+    //            using (var scope = host.Services.CreateScope())
+    //            {
+    //                var services = scope.ServiceProvider;
+    //                try
+    //                {
+    //                    InitializeDatabase(services);
+    //                }
+    //                catch (Exception ex)
+    //                {
+    //                    // something bad happened
+    //                }
+    //            }
+    //            host.Run();
+    //            return 0;
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            //return 1;
+    //            return 0;
+    //        }
+    //    }
+    //    public static IWebHost BuildWebHost(string[] args) =>
+    //    WebHost.CreateDefaultBuilder(args)
+    //    .UseStartup<Startup>()
+    //    .UseUrls("http://*:5000")
+    //    .Build();
+    //    private static void InitializeDatabase(IServiceProvider services)
+    //    {
+    //        using (var serviceScope = services.GetService<IServiceScopeFactory>().CreateScope())
+    //        {
+    //            var context = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    //            context.Database.Migrate();
+    //        }
+    //    }
+    //}
 }
