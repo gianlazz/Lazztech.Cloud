da2e18c50f6bcc30e2ea2106dc9d6c655ac68206
Gian Lazzarini <gianlazzarini@gmail.com>
2018-11-20 17:39:21 -0800

    Added Marten DB and SimpleDataAccess example code for postgresql document data storage with .net

---
+++ Lazztech.ObsidianPresences.ClientFacade/Dal/SimpleValue.cs
@@ -0,0 +1,8 @@
+﻿namespace Lazztech.ObsidianPresences.ClientFacade.Dal
+{
+    public class SimpleValue
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+    }
+}
