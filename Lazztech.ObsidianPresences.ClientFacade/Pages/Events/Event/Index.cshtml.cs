85dda1b8d06181662103a32ecc097e983e1f3114
Gian Lazzarini <gianlazzarini@gmail.com>
2018-11-22 15:39:36 -0800

    Added event index from HackathonHandler and link on the Events index

---
+++ Lazztech.ObsidianPresences.ClientFacade/Pages/Events/Event/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HackathonManager.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lazztech.ObsidianPresences.ClientFacade.Pages.Events.Event
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public List<Mentor> Mentors { get; set; }
+
+        public IndexModel()
+        {
+            Mentors = new List<Mentor>();
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
