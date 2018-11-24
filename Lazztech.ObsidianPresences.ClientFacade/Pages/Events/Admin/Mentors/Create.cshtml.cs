def2b4964afcace6a6c3ec90e5e81f3ce302e113
Gian Lazzarini <gianlazzarini@gmail.com>
2018-11-23 18:04:23 -0800

    Got the Mentors edit link working and worked on the Mentor Create page

--- Lazztech.ObsidianPresences.ClientFacade/Pages/Events/Admin/Mentors/Create.cshtml.cs
+++ Lazztech.ObsidianPresences.ClientFacade/Pages/Events/Admin/Mentors/Create.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HackathonManager.DTO;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,7 +11,15 @@ namespace Lazztech.ObsidianPresences.ClientFacade.Pages.Events.Admin.Mentors
 {
     public class CreateModel : PageModel
     {
+        [BindProperty]
+        public Mentor Mentor { get; set; }
+
         public void OnGet()
+        {
+            Mentor = new Mentor();
+        }
+
+        public void OnPost(FormCollection collection)
         {
 
         }
