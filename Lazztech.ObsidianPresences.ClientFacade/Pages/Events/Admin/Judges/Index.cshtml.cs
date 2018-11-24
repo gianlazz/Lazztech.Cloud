fe223e218048432491cf6a286486a6c691b9adff
Gian Lazzarini <gianlazzarini@gmail.com>
2018-11-23 16:00:21 -0800

    Scaffolded a folder and index for the Judges, Sms & Teams pages in the Events/Admin/ section

---
+++ Lazztech.ObsidianPresences.ClientFacade/Pages/Events/Admin/Judges/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lazztech.ObsidianPresences.ClientFacade.Pages.Events.Admin.Judges
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
