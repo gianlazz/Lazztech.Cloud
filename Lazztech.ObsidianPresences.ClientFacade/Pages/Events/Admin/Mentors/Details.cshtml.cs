9c6320bdba85d6d7addd9cf0f7c1e78dbf37199c
Gian Lazzarini <gianlazzarini@gmail.com>
2018-12-01 14:40:05 -0800

    Got the Mentors Details page working

--- Lazztech.ObsidianPresences.ClientFacade/Pages/Events/Admin/Mentors/Details.cshtml.cs
+++ Lazztech.ObsidianPresences.ClientFacade/Pages/Events/Admin/Mentors/Details.cshtml.cs
@@ -7,17 +7,19 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using HackathonManager.DTO;
 using Lazztech.ObsidianPresences.ClientFacade.Data;
+using HackathonManager.RepositoryPattern;
 
 namespace Lazztech.ObsidianPresences.ClientFacade.Pages.Events.Admin.Mentors
 {
     public class DetailsModel : PageModel
     {
-        private readonly Lazztech.ObsidianPresences.ClientFacade.Data.ApplicationDbContext _context;
+        //private readonly Lazztech.ObsidianPresences.ClientFacade.Data.ApplicationDbContext _context;
 
-        public DetailsModel(Lazztech.ObsidianPresences.ClientFacade.Data.ApplicationDbContext context)
-        {
-            _context = context;
-        }
+        //public DetailsModel(Lazztech.ObsidianPresences.ClientFacade.Data.ApplicationDbContext context)
+        //{
+        //    _context = context;
+        //}
+        private IRepository _repo = Startup.DbRepo;
 
         public Mentor Mentor { get; set; }
 
@@ -28,7 +30,8 @@ namespace Lazztech.ObsidianPresences.ClientFacade.Pages.Events.Admin.Mentors
                 return NotFound();
             }
 
-            Mentor = await _context.Mentor.FirstOrDefaultAsync(m => m.Id == id);
+            //Mentor = await _context.Mentor.FirstOrDefaultAsync(m => m.Id == id);
+            Mentor = _repo.All<Mentor>().FirstOrDefault(m => m.Id == id);
 
             if (Mentor == null)
             {
