62bba98bbe912d418ceec7711349a375dcbe3e3c
Gian Lazzarini <gianlazzarini@gmail.com>
2018-12-02 19:39:20 -0800

    Setup Events/Event/Judges page front end styling

--- Lazztech.ObsidianPresences.ClientFacade/Pages/Events/Event/Judges.cshtml.cs
+++ Lazztech.ObsidianPresences.ClientFacade/Pages/Events/Event/Judges.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HackathonManager;
+using HackathonManager.RepositoryPattern;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +11,14 @@ namespace Lazztech.ObsidianPresences.ClientFacade.Pages.Events.Event
 {
     public class JudgesModel : PageModel
     {
+        private IRepository _Db = Startup.DbRepo;
+
+
+        public List<Judge> Judges { get; set; }
+
         public void OnGet()
         {
-
+            Judges = _Db.All<Judge>().ToList();
         }
     }
 }
