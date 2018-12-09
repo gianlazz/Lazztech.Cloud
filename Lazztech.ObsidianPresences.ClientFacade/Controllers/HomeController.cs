ee7ec59dc07b4e20e83174500fa73abfbd64ffc7
Gian Lazzarini <gianlazzarini@gmail.com>
2018-12-09 14:28:03 -0800

    Added HomeController and trying to get the Html.BeginForm working for login for the events

---
+++ Lazztech.ObsidianPresences.ClientFacade/Controllers/HomeController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HackathonManager.DTO;
+using HackathonManager.Models;
+using HackathonManager.PocoModels;
+using HackathonManager.Sms;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lazztech.ObsidianPresences.ClientFacade.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult TeamLogin(int teamPin)
+        {
+            var Db = Startup.DbRepo;
+
+            var cookie = Request.Cookies["team"];
+
+            //CHECK IF A TEAM BY THAT PIN NUMBER EXSISTS
+            if (Db.Single<Team>(x => x.PinNumber == teamPin) != null)
+            {
+                Team team = Db.Single<Team>(x => x.PinNumber == teamPin);
+
+                if (cookie == null)
+                {
+                    //var option = new CookieOptions();
+                    //option.Expires = DateTime.UtcNow.AddDays(3);
+                    //Response.Cookies["team"].Value = team.Name;
+                    //Response.Cookies.Append("string", team.Name, option);
+                    Response.Cookies.Append("team", team.Name, new CookieOptions() { Expires = DateTime.UtcNow.AddDays(3) });
+
+                    //Response.Cookies["team"].Expires = DateTime.UtcNow.AddDays(3);
+                }
+                if (cookie != null && cookie.ToString() == "")
+                {
+                    //Response.Cookies["team"].Value = team.Name;
+                    //Response.Cookies["team"].Expires = DateTime.UtcNow.AddDays(3);
+                    Response.Cookies.Append("team", team.Name, new CookieOptions() { Expires = DateTime.UtcNow.AddDays(3) });
+                }
+                //if (cookie != null && cookie.Value == "")
+                //{
+                //    Response.Cookies["team"].Value = team.Name;
+                //    Response.Cookies["team"].Expires = DateTime.UtcNow.AddDays(3);
+                //}
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        //public ActionResult LogOut()
+        //{
+        //    HttpCookie cookie = Request.Cookies["team"];
+        //    if (cookie != null)
+        //    {
+        //        Response.Cookies["team"].Value = "";
+        //    }
+        //    else
+        //    {
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return RedirectToAction("Index");
+        //}
+
+        [HttpPost]
+        public ActionResult MentorRequest(int teamPin, Guid mentorGuidId)
+        {
+            var Db = Startup.DbRepo;
+            var sms = Startup.SmsService;
+            var request = new MentorRequest();
+            Team team = null;
+            Mentor mentor = null;
+            try
+            {
+                team = Db.Single<Team>(x => x.PinNumber == teamPin);
+                mentor = Db.Single<Mentor>(x => x.Id == mentorGuidId);
+            }
+            catch (Exception ex)
+            {
+                Db.Add<Log>(new Log() { Details = ex.ToString() });
+            }
+
+            request.Team = team;
+            request.Mentor = mentor;
+            var message = $"🔥 { mentor.FirstName}, team { team.Name}, located in { team.Location}, has requested your assistance.\n\n" +
+    $"Reply with:\n" +
+    $"Y to accept " +
+    $"\nor\n " +
+    $"N to reject the request";
+
+            try
+            {
+                request.OutboundSms = sms.SendSms(mentor.PhoneNumber, message);
+            }
+            catch (Exception ex)
+            {
+                //IS THROWING EXCEPTION FOR NOT HAVING CORRECTLY SETUP NEWTONSOFT.JSON DEPENDENCY
+                Db.Add<Log>(new Log() { Details = ex.ToString() });
+            }
+
+            try
+            {
+                SmsRoutingConductor.MentorRequests.Add(request);
+
+                //THIS SHOULD BE HANDLED BY THE SMSROUTINGCONDUCTOR
+                Db.Add<MentorRequest>(request);
+            }
+            catch (Exception ex)
+            {
+                Db.Add<Log>(new Log() { Details = ex.ToString() });
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
