768ed9a2ddcc0ca5ee386743e4a417adb55e9f56
Gian Lazzarini <gianlazzarini@gmail.com>
2019-01-08 18:02:00 -0800

    Made MongoDBLogger specific ILogger implementation that doesn't use the repository pattern

---
+++ Lazztech.MongoDB/MongoDBLogger.cs
@@ -0,0 +1,29 @@
+﻿using HackathonManager;
+using HackathonManager.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lazztech.MongoDB
+{
+    class MongoDBLogger : ILogger
+    {
+        private IMongoDatabase _db;
+
+        public MongoDBLogger(IMongoDatabase mongoDatabase)
+        {
+            _db = mongoDatabase;
+        }
+
+        public void Log(Exception exception)
+        {
+            _db.GetCollection<Log>(typeof(Log).Name).InsertOne(new Log() { Details = exception.ToString() });
+        }
+
+        public void Log(string s)
+        {
+            _db.GetCollection<Log>(typeof(Log).Name).InsertOne(new Log() { Details = s});
+        }
+    }
+}
