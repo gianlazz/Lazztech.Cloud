a9cc66cb3fb13e4230188ed30907f7b2d19fa1ea
Gian Lazzarini <gianlazzarini@gmail.com>
2019-01-09 18:58:00 -0800

    Installed Moq to the testing layers

--- Lazztech.Events.Tests/SmsRoutingConductorTests.cs
+++ Lazztech.Events.Tests/SmsRoutingConductorTests.cs
@@ -1,3 +1,6 @@
+using Lazztech.Events.Domain.Sms;
+using Lazztech.Events.Dto.Interfaces;
+using Moq;
 using System;
 using Xunit;
 
@@ -8,7 +11,15 @@ namespace Lazztech.Events.Tests
         [Fact]
         public void Test1()
         {
+            //Arrange
+            var repo = new Mock<IRepository>();
+            var sms = new Mock<ISmsService>();
+            var responder = new Mock<IRequestResponder>();
+            var conductor = new SmsRoutingConductor(repo.Object, sms.Object, responder.Object);
 
+            //Act
+
+            //Assert
         }
     }
 }
