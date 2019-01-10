ffc397d6cd8a499780f8c0b801610ebd96b494eb
Gian Lazzarini <gianlazzarini@gmail.com>
2019-01-09 20:45:48 -0800

    Refactored out the SmsRoutingConductorTests helper methods and moved the arrangment to just the inside of the first test

--- Lazztech.Events.Tests/SmsRoutingConductorTests.cs
+++ Lazztech.Events.Tests/SmsRoutingConductorTests.cs
@@ -5,20 +5,37 @@ using Lazztech.Events.Dto.Models;
 using Moq;
 using System;
 using Xunit;
+using System.Linq;
 
 namespace Lazztech.Events.Tests
 {
     public class SmsRoutingConductorTests
     {
         [Fact]
-        public void MentorRequestAndAcceptance()
+        public void ProcessRequestAcceptanceWithNoNullDateTimeWhenProcessed()
         {
             //Arrange
             var repo = new Mock<IRepository>();
             var sms = new Mock<ISmsService>();
             var responder = new Mock<IRequestResponder>();
             var conductor = new SmsRoutingConductor(repo.Object, sms.Object, responder.Object);
-            var request = MentorRequestHelper("exampleTeam");
+
+            var request = new MentorRequest()
+            {
+                TeamName = "TestTeamName123",
+                Mentor = new Mentor()
+                {
+                    FirstName = "Gian",
+                    LastName = "Lazzarini",
+                    PhoneNumber = "GiansNumber123",
+                },
+                OutboundSms = new SmsDto(
+                    EventStrings.OutBoundRequestSms("Gian", "exampleTeam", "Example Room"),
+                    toNumber: "GiansNumber123", 
+                    fromNumber: "TwilioNumber123"
+                    ),
+            };
+
             var smsResponse = new SmsDto(message: "Y", toNumber: "TwilioNumber123", fromNumber: "GiansNumber123");
 
             //Act
@@ -27,23 +44,8 @@ namespace Lazztech.Events.Tests
             conductor.ProcessMentorRequests();
 
             //Assert
-            //Assert.True();
-        }
-
-        public MentorRequest MentorRequestHelper(string team)
-        {
-            var request = new MentorRequest();
-            request.TeamName = "TestTeamName123";
-            request.Mentor = new Mentor()
-            {
-                FirstName = "Gian",
-                LastName = "Lazzarini",
-                PhoneNumber = "GiansNumber123",
-            };
-            var message = EventStrings.OutBoundRequestSms("Gian", team, "Example Room");
-            request.OutboundSms = new SmsDto(message: message, toNumber: "GiansNumber123", fromNumber: "TwilioNumber123");
-
-            return request;
+            Assert.DoesNotContain(SmsRoutingConductor.InboundMessages, x => x.DateTimeWhenProcessed == null);
+            Assert.DoesNotContain(SmsRoutingConductor.MentorRequests, x => x.DateTimeWhenProcessed == null);
         }
     }
 }
