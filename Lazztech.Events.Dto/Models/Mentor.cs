7ae119c1361e1adef3e82887bf456b19284d8cef
Gian Lazzarini <gianlazzarini@gmail.com>
2019-01-19 12:13:27 -0800

    Removed unused proeprties from the mentor dto

--- Lazztech.Events.Dto/Models/Mentor.cs
+++ Lazztech.Events.Dto/Models/Mentor.cs
@@ -9,18 +9,15 @@ namespace Lazztech.Events.Dto.Models
     public class Mentor
     {
         public Guid Id { get; set; }
-        public string Event { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Image { get; set; }
-        public int Age { get; set; }
         public string Description { get; set; }
         public string ProfessionalTitle { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
         public bool IsAvailable { get; set; }
         public bool IsPresent { get; set; }
-        public string MentorType { get; set; }
 
         public Mentor()
         {
