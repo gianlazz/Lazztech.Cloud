a9fa3c12225dac78b26d9ea80e63d81d6d9a50a0
Gian Lazzarini <gianlazzarini@gmail.com>
2019-01-20 19:06:31 -0800

    Cleaned up the ProgressHub

--- Lazztech.Cloud.ClientFacade/Hubs/ProgressHub.cs
+++ Lazztech.Cloud.ClientFacade/Hubs/ProgressHub.cs
@@ -8,6 +8,28 @@ namespace Lazztech.Cloud.ClientFacade.Hubs
 {
     public class ProgressHub : Hub
     {
+        public override Task OnConnectedAsync()
+        {
+            string uniqueUsersCookie = null;
+            if (Context.GetHttpContext().Request.Cookies.ContainsKey(StaticStrings.eventUserIdCookieName))
+                uniqueUsersCookie = Context.GetHttpContext().Request.Cookies[StaticStrings.eventUserIdCookieName];
+            if (uniqueUsersCookie != null)
+                Groups.AddToGroupAsync(Context.ConnectionId, uniqueUsersCookie);
+
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public void UpdateTeamOfMentorRequest(string UniqueUserId, bool accepted, string message = null)
+        {
+            var percentage = (20 * 100) / 100;
+            Clients.Group(UniqueUserId).SendAsync("requestUpdate", message, percentage);
+        }
+
         public void UpdateTeam(MentorRequest request, string message)
         {
             if (request.RequestAccepted)
@@ -24,90 +46,9 @@ namespace Lazztech.Cloud.ClientFacade.Hubs
             });
         }
 
-        public void AddConnectionToTeamGroup()
-        {
-            var teamCookieValue = Context.GetHttpContext().Request.Cookies[StaticStrings.eventUserIdCookieName];
-
-            if (!string.IsNullOrWhiteSpace(teamCookieValue))
-            {
-                Groups.AddToGroupAsync(Context.ConnectionId, teamCookieValue);
-            }
-        }
-
         public void MessageTeam(Team team, string message)
         {
             Clients.Group(team.Name).SendAsync("message", message);
-        }
-
-        public static ConcurrentDictionary<string, Team> MyUsers = new ConcurrentDictionary<string, Team>();
-
-        public override Task OnConnectedAsync()
-        {
-            string usersTeamCookie = null;
-            if (Context.GetHttpContext().Request.Cookies.ContainsKey(StaticStrings.eventUserIdCookieName))
-                usersTeamCookie = Context.GetHttpContext().Request.Cookies[StaticStrings.eventUserIdCookieName];
-            if (usersTeamCookie != null)
-            {
-                MyUsers.TryAdd(Context.ConnectionId, new Team() { Name = usersTeamCookie });
-                //string name = Context.User.Identity.Name;
-
-                //Groups.Add(Context.ConnectionId, name);
-                Groups.AddToGroupAsync(Context.ConnectionId, usersTeamCookie);
-
-                if (usersTeamCookie != null)
-                {
-                    MyUsers.TryAdd(Context.ConnectionId, new Team() { Name = usersTeamCookie });
-                    //string name = Context.User.Identity.Name;
-
-                    //Groups.Add(Context.ConnectionId, name);
-
-                    //Groups.Add(Context.ConnectionId, cookie.Value);
-                    Groups.AddToGroupAsync(Context.ConnectionId, usersTeamCookie);
-                }
-            }
-
-            return base.OnConnectedAsync();
-        }
-
-        public override Task OnDisconnectedAsync(Exception exception)
-        {
-            Team team;
-            MyUsers.TryRemove(Context.ConnectionId, out team);
-            return base.OnDisconnectedAsync(exception);
-        }
-
-        //public override Task OnReconnected()
-        //{
-        //    Cookie cookie = Context.Request.Cookies[StaticStrings.eventUserIdCookieName];
-        //    if (cookie != null)
-        //    {
-        //        MyUsers.TryAdd(Context.ConnectionId, new Team() { Name = cookie.Value });
-        //        //string name = Context.User.Identity.Name;
-
-        //        //Groups.Add(Context.ConnectionId, name);
-        //        Groups.Add(Context.ConnectionId, cookie.Name);
-
-        //        if (cookie.Value != null)
-        //        {
-        //            MyUsers.TryAdd(Context.ConnectionId, new Team() { Name = cookie.Value });
-        //            //string name = Context.User.Identity.Name;
-
-        //            //Groups.Add(Context.ConnectionId, name);
-        //            Groups.Add(Context.ConnectionId, cookie.Name);
-        //        }
-        //    }
-
-        //    return base.OnReconnected();
-        //}
-
-        public void UpdateTeamOfMentorRequest(string teamName, bool accepted, string message = null)
-        {
-            var percentage = (20 * 100) / 100;
-
-            //PUSHING DATA TO ALL CLIENTS
-
-            //Clients.Group(teamName).requestUpdate(message, percentage);
-            Clients.Group(teamName).SendAsync("requestUpdate", message, percentage);
         }
     }
 }
