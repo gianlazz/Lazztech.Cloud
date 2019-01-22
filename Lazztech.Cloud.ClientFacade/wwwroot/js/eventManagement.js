9038aff964e1d237c013cf757fbfc90295d1cbd3
Gian Lazzarini <gianlazzarini@gmail.com>
2019-01-21 18:29:29 -0800

    Moved old signalr js code to the eventManagement.js and started porting it over with a method for debugging which throws an exception on the server hub ThrowException()

--- Lazztech.Cloud.ClientFacade/wwwroot/js/eventManagement.js
+++ Lazztech.Cloud.ClientFacade/wwwroot/js/eventManagement.js
@@ -2,23 +2,55 @@
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/progressHub").build();
 
-connection.on("ReceiveMessage", function (user, message) {
-    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var encodedMsg = user + " says " + msg;
-    var li = document.createElement("li");
-    li.textContent = encodedMsg;
-    document.getElementById("messagesList").appendChild(li);
-});
+//connection.on("ReceiveMessage", function (user, message) {
+//    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+//    var encodedMsg = user + " says " + msg;
+//    var li = document.createElement("li");
+//    li.textContent = encodedMsg;
+//    document.getElementById("messagesList").appendChild(li);
+//});
 
 connection.start().catch(function (err) {
     return console.error(err.toString());
 });
+setTimeout(function () {
+    connection.invoke("ThrowException").catch(err => console.error(err.toString()));
+}, 1500);
 
-document.getElementById("sendButton").addEventListener("click", function (event) {
-    var user = document.getElementById("userInput").value;
-    var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(function (err) {
-        return console.error(err.toString());
-    });
-    event.preventDefault();
+
+
+// Reference the auto-generated proxy for the hub.
+//var progress = $.connection.progressHub;
+//console.log(progress);
+
+// Create a function that the hub can call back to display messages.
+connection.client.addProgress = function (message, percentage) {
+    //at this point server side had send message and percentage back to the client
+    //and then we handle progress bar any way we want it
+
+    //Using a function in Helper.js file we show modal and display text and percentage
+    ProgressBarModal("show", message + " " + percentage);
+
+    //We're filling blue progress indicator by setting the width property to the variable
+    //that was returned from the server
+    $('#ProgressMessage').width(percentage);
+
+    //closing modal when the progress gets to 100%
+    if (percentage === "100%") {
+        ProgressBarModal();
+    }
+};
+
+//MENTOR REQUEST UPDATE
+connection.client.requestUpdate = function (message) {
+    MentorRequestUpdateModal(message);
+};
+
+//Before doing anything with our hub we must start it
+$.connection.hub.start().done(function () {
+
+    //getting the connection ID in case you want to display progress to the specific user
+    //that started the process in the first place.
+    var connectionId = $.connection.hub.id;
+    console.log(new { Message: "Connection Id:", connectionId });
 });
