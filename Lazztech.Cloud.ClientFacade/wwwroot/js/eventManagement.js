3f6ea992f59d5ae956b55844b8262e9b4a742cef
Gian Lazzarini <gianlazzarini@gmail.com>
2019-01-21 19:21:02 -0800

    Got Event SignalR notifications working after adding in missing MentorRequestUpdateModal() js method

--- Lazztech.Cloud.ClientFacade/wwwroot/js/eventManagement.js
+++ Lazztech.Cloud.ClientFacade/wwwroot/js/eventManagement.js
@@ -2,6 +2,17 @@
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/progressHub").build();
 
+connection.start().then(() => {
+    console.log(connection);
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+//For debugging shows example of how to await connection start then invoke server side method on the hub
+//connection.start().then(() => {
+//    connection.invoke("ThrowException").catch(err => console.error(err.toString()));
+//});
+
 //connection.on("ReceiveMessage", function (user, message) {
 //    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
 //    var encodedMsg = user + " says " + msg;
@@ -10,47 +21,61 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/progressHub").buil
 //    document.getElementById("messagesList").appendChild(li);
 //});
 
-connection.start().catch(function (err) {
-    return console.error(err.toString());
-});
-setTimeout(function () {
-    connection.invoke("ThrowException").catch(err => console.error(err.toString()));
-}, 1500);
-
-
-
-// Reference the auto-generated proxy for the hub.
-//var progress = $.connection.progressHub;
-//console.log(progress);
-
 // Create a function that the hub can call back to display messages.
-connection.client.addProgress = function (message, percentage) {
-    //at this point server side had send message and percentage back to the client
-    //and then we handle progress bar any way we want it
+connection.on("requestUpdate", (message) => {
+    MentorRequestUpdateModal(message);
+});
 
-    //Using a function in Helper.js file we show modal and display text and percentage
-    ProgressBarModal("show", message + " " + percentage);
+function ProgressBarModal(showHide) {
 
-    //We're filling blue progress indicator by setting the width property to the variable
-    //that was returned from the server
-    $('#ProgressMessage').width(percentage);
+    if (showHide === 'show') {
+        $('#mod-progress').modal('show');
+        if (arguments.length >= 2) {
+            $('#progressBarParagraph').text(arguments[1]);
+        } else {
+            $('#progressBarParagraph').text('U tijeku...');
+        }
 
-    //closing modal when the progress gets to 100%
-    if (percentage === "100%") {
-        ProgressBarModal();
+        window.progressBarActive = true;
+
+    } else {
+        $('#mod-progress').modal('hide');
+        window.progressBarActive = false;
     }
-};
+}
+
+function MentorRequestUpdateModal() {
+    $('#mentorRequestUpdateModal').modal('show');
+    $('#mentorRequestUpdateParagraph').text(arguments[0]);
+}
+
+//connection.client.addProgress = function (message, percentage) {
+//    //at this point server side had send message and percentage back to the client
+//    //and then we handle progress bar any way we want it
+
+//    //Using a function in Helper.js file we show modal and display text and percentage
+//    ProgressBarModal("show", message + " " + percentage);
+
+//    //We're filling blue progress indicator by setting the width property to the variable
+//    //that was returned from the server
+//    $('#ProgressMessage').width(percentage);
+
+//    //closing modal when the progress gets to 100%
+//    if (percentage === "100%") {
+//        ProgressBarModal();
+//    }
+//};
 
 //MENTOR REQUEST UPDATE
-connection.client.requestUpdate = function (message) {
-    MentorRequestUpdateModal(message);
-};
+//connection.client.requestUpdate = function (message) {
+//    MentorRequestUpdateModal(message);
+//};
 
 //Before doing anything with our hub we must start it
-$.connection.hub.start().done(function () {
+//$.connection.hub.start().done(function () {
 
-    //getting the connection ID in case you want to display progress to the specific user
-    //that started the process in the first place.
-    var connectionId = $.connection.hub.id;
-    console.log(new { Message: "Connection Id:", connectionId });
-});
+//    //getting the connection ID in case you want to display progress to the specific user
+//    //that started the process in the first place.
+//    var connectionId = $.connection.hub.id;
+//    console.log(new { Message: "Connection Id:", connectionId });
+//});
