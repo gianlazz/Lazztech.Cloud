5aed3145ce7015726b0cbc5ed802b35eeea2c6ad
Gian Lazzarini <gianlazzarini@gmail.com>
2019-01-25 14:24:42 -0800

    Added Events/Invites page and made the MentorIntives table responsive for small displays

---
+++ Lazztech.Cloud.ClientFacade/Pages/Events/Invites.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lazztech.Cloud.ClientFacade.Pages.Events
+{
+    public class InvitesModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
