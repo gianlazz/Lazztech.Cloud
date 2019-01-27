473eb7dc4a46e3784fd38c3e0c4c351e7da056e6
Gian Lazzarini <gianlazzarini@gmail.com>
2019-01-26 18:26:34 -0800

    Created Lazztech.Standard FileService : IFileService

---
+++ Lazztech.Standard/Interfaces/IFileService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lazztech.Standard.Interfaces
+{
+    public interface IFileService
+    {
+        bool Exists(string path);
+        void CreateDirectory(string path);
+        void WriteAllBytes(string path, byte[] bytes);
+        void WriteAllText(string path, string content);
+    }
+}
---
+++ Lazztech.Standard/Services/FileService.cs
@@ -0,0 +1,37 @@
+﻿using Lazztech.Standard.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Lazztech.Standard.Services
+{
+    public class FileService : IFileService
+    {
+        public void CreateDirectory(string path)
+        {
+            Directory.CreateDirectory(path);
+        }
+
+        public bool Exists(string path)
+        {
+            return Directory.Exists(path);
+        }
+
+        public void WriteAllBytes(string path, byte[] bytes)
+        {
+            var dir = Path.GetDirectoryName(path);
+            if (!Exists(dir))
+                CreateDirectory(dir);
+            File.WriteAllBytes(path, bytes);
+        }
+
+        public void WriteAllText(string path, string content)
+        {
+            var dir = Path.GetDirectoryName(path);
+            if (!Exists(dir))
+                CreateDirectory(dir);
+            File.WriteAllText(path, content);
+        }
+    }
+}
