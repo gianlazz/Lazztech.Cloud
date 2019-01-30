672675d40848b6928d0d03a25a5325558adcaf0e
Gian Lazzarini <gianlazzarini@gmail.com>
2019-01-29 20:14:26 -0800

    Updated the docker composes for both dev and the rpi

--- Lazztech.Cloud.ClientFacade/Program.cs
+++ Lazztech.Cloud.ClientFacade/Program.cs
@@ -13,58 +13,50 @@ using Microsoft.Extensions.Logging;
 
 namespace Lazztech.Cloud.ClientFacade
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-    }
-
     //public class Program
     //{
-    //    public static int Main(string[] args)
+    //    public static void Main(string[] args)
     //    {
-    //        try
-    //        {
-    //            var host = BuildWebHost(args);
-    //            using (var scope = host.Services.CreateScope())
-    //            {
-    //                var services = scope.ServiceProvider;
-    //                try
-    //                {
-    //                    InitializeDatabase(services);
-    //                }
-    //                catch (Exception ex)
-    //                {
-    //                    // something bad happened
-    //                }
-    //            }
-    //            host.Run();
-    //            return 0;
-    //        }
-    //        catch (Exception ex)
-    //        {
-    //            //return 1;
-    //            return 0;
-    //        }
+    //        CreateWebHostBuilder(args).Build().Run();
     //    }
-    //    public static IWebHost BuildWebHost(string[] args) =>
-    //    WebHost.CreateDefaultBuilder(args)
-    //    .UseStartup<Startup>()
-    //    .UseUrls("http://*:5000")
-    //    .Build();
-    //    private static void InitializeDatabase(IServiceProvider services)
-    //    {
-    //        using (var serviceScope = services.GetService<IServiceScopeFactory>().CreateScope())
-    //        {
-    //            var context = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    //            context.Database.Migrate();
-    //        }
-    //    }
+
+    //    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+    //        WebHost.CreateDefaultBuilder(args)
+    //            .UseStartup<Startup>();
     //}
+
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            try
+            {
+                var host = CreateWebHostBuilder(args);
+                using (var scope = host.Services.CreateScope())
+                    InitializeDatabase(scope.ServiceProvider);
+                host.Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                //return 1;
+                return 0;
+            }
+        }
+
+        public static IWebHost CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+
+
+        private static void InitializeDatabase(IServiceProvider services)
+        {
+            using (var serviceScope = services.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                context.Database.Migrate();
+            }
+        }
+    }
 }
