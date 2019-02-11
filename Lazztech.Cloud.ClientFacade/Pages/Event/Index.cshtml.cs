9e3d56a2ead326d0ac9f6abd24b6691369a3ab3e
Gian Lazzarini <gianlazzarini@gmail.com>
2019-02-11 15:07:05 -0800

    Setup the new efcore based Event/Index page along with eager loading of the event organization and an eventId param on the OnGet method

---
+++ Lazztech.Cloud.ClientFacade/Pages/Event/Index.cshtml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lazztech.Cloud.ClientFacade.Data;
+using Lazztech.Events.Dal.Dao;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lazztech.Cloud.ClientFacade.Pages.Event
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public Organization Organization { get; set; }
+        [BindProperty]
+        public Lazztech.Events.Dal.Dao.Event Event { get; set; }
+        [BindProperty]
+        public List<Mentor> Mentors { get; set; }
+        [BindProperty]
+        public string UniqueRequesteeId { get; set; }
+        public string Message { get; set; }
+        public string Alert { get; set; }
+
+        private readonly ApplicationDbContext _context;
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+
+            Mentors = new List<Mentor>();
+        }
+
+        public IActionResult OnGet(string message, string alert, int eventId)
+        {
+            Message = message;
+            Alert = alert;
+
+            Event = _context.Events.Include(x => x.Organization).FirstOrDefault(x => x.EventId == eventId);
+            if (Event == null)
+                return NotFound();
+            Organization = Event.Organization;
+
+            if (Request.Cookies.ContainsKey(StaticStrings.eventUserIdCookieName))
+                UniqueRequesteeId = Request.Cookies[StaticStrings.eventUserIdCookieName];
+
+            Mentors = _context.Mentors
+                .Where(x => x.EventMentors.FirstOrDefault(y => y.EventId == eventId) != null)
+                .OrderBy(z => z.IsAvailable ? 0 : 1).ToList();
+
+            return Page();
+        }
+    }
+}
