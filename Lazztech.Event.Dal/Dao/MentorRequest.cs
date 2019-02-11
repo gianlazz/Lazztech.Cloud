c5c80a657b003d45d558a064af31f98c7ea6b5bf
Gian Lazzarini <gianlazzarini@gmail.com>
2019-02-10 16:58:34 -0800

    Working on EfDalHelper and added MentorRequest Dao

---
+++ Lazztech.Event.Dal/Dao/MentorRequest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lazztech.Events.Dal.Dao
+{
+    public class MentorRequest
+    {
+        public int MentorRequestId { get; set; }
+        public DateTime DateTimeOfRequest { get; set; }
+
+        public int MentorId { get; set; }
+        public Mentor Mentor { get; set; }
+
+        public string UniqueRequesteeId { get; set; }
+        public bool RequestAccepted { get; set; }
+        public DateTime? DateTimeWhenProcessed { get; set; }
+        public Sms OutboundSms { get; set; }
+        public Sms InboundSms { get; set; }
+        public TimeSpan MentoringDuration { get; set; }
+        public TimeSpan RequestTimeout { get; set; }
+        public bool TimedOut { get; set; }
+
+        public MentorRequest()
+        {
+            DateTimeOfRequest = DateTime.Now;
+
+            MentoringDuration = new TimeSpan(hours: 0, minutes: 12, seconds: 0);
+            RequestTimeout = new TimeSpan(hours: 0, minutes: 5, seconds: 0);
+        }
+    }
+}
--- Lazztech.Event.Dal/EfDalHelper.cs
+++ Lazztech.Event.Dal/EfDalHelper.cs
@@ -1,19 +1,61 @@
 ﻿using Lazztech.Events.Dto.Interfaces;
 using Lazztech.Events.Dto.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace Lazztech.Event.Dal
 {
     public class EfDalHelper : IDalHelper
     {
+        private readonly DbContext _db;
+
+        public EfDalHelper(DbContext dbContext)
+        {
+            _db = dbContext;
+        }
+
+        public void Add<T>(T item) where T : class, new()
+        {
+            _db.Add(item);
+            _db.SaveChanges();
+        }
+
+        public void Add<T>(IEnumerable<T> items) where T : class, new()
+        {
+            _db.AddRange(items);
+            _db.SaveChanges();
+        }
+
         public void AddMenorRequestDb(MentorRequest request)
         {
             throw new NotImplementedException();
         }
 
         public void AddSmsDb(SmsDto inboundSms)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> All<T>() where T : class, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete<T>(Expression<Func<T, bool>> expression) where T : class, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete<T>(T item) where T : class, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Single<T>(Expression<Func<T, bool>> expression) where T : class, new()
         {
             throw new NotImplementedException();
         }
