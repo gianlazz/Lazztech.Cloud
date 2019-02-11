8c7d9a62be2db89f39631b7c7c1b2ae8c06f95d5
Gian Lazzarini <gianlazzarini@gmail.com>
2019-02-10 17:47:47 -0800

    Generated efcore crud pages for Events

---
+++ Lazztech.Cloud.ClientFacade/Pages/Events/Admin/Events/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Lazztech.Cloud.ClientFacade.Data;
+using Lazztech.Events.Dal.Dao;
+
+namespace Lazztech.Cloud.ClientFacade.Pages.Events.Admin.Events
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Lazztech.Cloud.ClientFacade.Data.ApplicationDbContext _context;
+
+        public CreateModel(Lazztech.Cloud.ClientFacade.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["OrganizationId"] = new SelectList(_context.Organizations, "OrganizationId", "OrganizationId");
+            return Page();
+        }
+
+        [BindProperty]
+        public Event Event { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Events.Add(Event);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Lazztech.Cloud.ClientFacade/Pages/Events/Admin/Events/Delete.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Lazztech.Cloud.ClientFacade.Data;
+using Lazztech.Events.Dal.Dao;
+
+namespace Lazztech.Cloud.ClientFacade.Pages.Events.Admin.Events
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Lazztech.Cloud.ClientFacade.Data.ApplicationDbContext _context;
+
+        public DeleteModel(Lazztech.Cloud.ClientFacade.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Event Event { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Event = await _context.Events
+                .Include(@ => @.Organization).FirstOrDefaultAsync(m => m.EventId == id);
+
+            if (Event == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Event = await _context.Events.FindAsync(id);
+
+            if (Event != null)
+            {
+                _context.Events.Remove(Event);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
