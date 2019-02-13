63fcfd4da326bcc06f7bd54cb2377b90a75dfe47
Gian Lazzarini <gianlazzarini@gmail.com>
2019-02-12 16:32:41 -0800

    Deleted old mongodb based Events/Admin/Mentors and built out the Admin/EventManagement index dashboard

--- Lazztech.Cloud.ClientFacade/Pages/Admin/EventManagement/Index.cshtml.cs
+++ Lazztech.Cloud.ClientFacade/Pages/Admin/EventManagement/Index.cshtml.cs
@@ -2,16 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Lazztech.Cloud.ClientFacade.Data;
+using Lazztech.Events.Dal.Dao;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Lazztech.Cloud.ClientFacade.Pages.Admin.EventManagement
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        public int OrganizationCount { get; set; }
+        public int EventCount { get; set; }
+        public int MentorCount { get; set; }
+        public List<Lazztech.Events.Dal.Dao.Sms> Messages { get; set; }
 
+        private readonly ApplicationDbContext _context;
+
+        public IndexModel(ApplicationDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+        }
+
+        public async Task OnGetAsync()
+        {
+            OrganizationCount = await _context.Organizations.CountAsync();
+            EventCount = await _context.Events.CountAsync();
+            MentorCount = await _context.Mentors.CountAsync();
+            Messages = await _context.SmsMessages.ToListAsync();
         }
     }
 }
