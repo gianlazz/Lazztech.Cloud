68782c111b83ad7d63f4ee65c702df6107ad6958
Gian Lazzarini <gianlazzarini@gmail.com>
2019-02-18 14:52:33 -0800

    EfConductorDalHelper: Fixed more explicit entity tracking, detatching and specifics of what's been modified

--- Lazztech.Cloud.ClientFacade/Data/EfConductorDalHelper.cs
+++ Lazztech.Cloud.ClientFacade/Data/EfConductorDalHelper.cs
@@ -62,7 +62,10 @@ namespace Lazztech.Cloud.ClientFacade.Data
         {
             var entity = mentor.MapToEntity();
             _context.Attach(entity).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
-            _context.Entry(entity).Property(x => x.Event).IsModified = false;
+
+            _context.Entry(entity).Reference(x => x.Event).IsModified = false;
+            //_context.Entry(entity).Property(x => x.Event).IsModified = false;
+
             _context.SaveChanges();
             _context.Entry(entity).State = EntityState.Detached;
         }
@@ -72,17 +75,21 @@ namespace Lazztech.Cloud.ClientFacade.Data
             var entity = request.MapToEntity();
             _context.Attach(entity).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
             _context.Attach(entity.Mentor).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
-            _context.Entry(entity).Property(x => x.Mentor.Event).IsModified = false;
 
-            _context.Attach(entity.Mentor.Event).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
-            _context.Attach(entity.Mentor.Event.Organization).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
-            _context.Attach(entity.OutboundSms).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
-            _context.Attach(entity.InboundSms).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.Entry(entity.Mentor).Reference(x => x.Event).IsModified = false;
+            //_context.Entry(entity).Property(x => x.Mentor.Event).IsModified = false;
+
+            if (entity.OutboundSms != null)
+                _context.Attach(entity.OutboundSms).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            if (entity.InboundSms != null)
+                _context.Attach(entity.InboundSms).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
             _context.SaveChanges();
             _context.Entry(entity).State = EntityState.Detached;
             _context.Entry(entity.Mentor).State = EntityState.Detached;
-            _context.Entry(entity.OutboundSms).State = EntityState.Detached;
-            _context.Entry(entity.InboundSms).State = EntityState.Detached;
+            if (entity.OutboundSms != null)
+                _context.Entry(entity.OutboundSms).State = EntityState.Detached;
+            if (entity.InboundSms != null)
+                _context.Entry(entity.InboundSms).State = EntityState.Detached;
         }
 
         public void UpdateSmsDb(SmsDto inboundSms)
