c1c7b9be8a56758a5942365314cb9ff161f272b7
Gian Lazzarini <gianlazzarini@gmail.com>
2019-03-05 18:47:40 -0800

    Added InstagramNode Dao & Dto

---
+++ Lazztech.Marketing.Dal/Dao/InstagramNode.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lazztech.Marketing.Dal.Dao
+{
+    public class InstagramNode
+    {
+        public int InstagramNodeId { get; set; }
+    }
+}
---
+++ Lazztech.Marketing.Dto/Models/InstagramNode.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lazztech.Marketing.Dto.Models
+{
+    public class InstagramNode
+    {
+        public int Id { get; set; }
+    }
+}
