437cbc4052a129681fb481db5f810c70c360ee59
Gian Lazzarini <gianlazzarini@gmail.com>
2019-03-09 17:25:33 -0800

    Working on FocusGroup index page

---
+++ Lazztech.Cloud.ClientFacade/Pages/Marketing/FocusGroup/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lazztech.Cloud.ClientFacade.Data;
+using Lazztech.Marketing.Dal.Dao;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lazztech.Cloud.ClientFacade.Pages.Marketing.FocusGroupPage
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        [BindProperty]
+        public FocusGroup FocusGroup { get; set; }
+
+        public IndexModel(ApplicationDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+        }
+
+        public void OnGet(int? Id)
+        {
+            if (Id != null)
+                FocusGroup = _context.FocusGroups.FirstOrDefault(x => x.FocusGroupId == Id);
+        }
+    }
+}
